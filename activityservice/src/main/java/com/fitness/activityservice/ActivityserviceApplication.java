2ed3f43cb065444753212f46bd0b048411dec5ec
30225885 <manavnarendrakumar.p@ucalgary.ca>
2025-07-30 21:42:56 -0600

    Established connection to MongoDB database

--- activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -1,5 +1,6 @@
 package com.fitness.activityservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ActivityserviceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MONGODB_CONNECTION_STRING", dotenv.get("MONGODB_CONNECTION_STRING"));
+		System.setProperty("MONGODB_DATABASE", dotenv.get("MONGODB_DATABASE"));
 		SpringApplication.run(ActivityserviceApplication.class, args);
 	}
-
 }
