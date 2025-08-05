d23f6b978e5ef34916c27b620fb9a4f9135b382b
30225885 <manavnarendrakumar.p@ucalgary.ca>
2025-08-05 15:36:40 -0600

    Point to config server for ai-service configurations

--- configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
@@ -11,6 +11,8 @@ public class ConfigserverApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MONGODB_CONNECTION_STRING", dotenv.get("MONGODB_CONNECTION_STRING"));
+		System.setProperty("MONGODB_DATABASE_AI", dotenv.get("MONGODB_DATABASE_AI"));
 		System.setProperty("POSTGRESQL_CONNECTION_STRING", dotenv.get("POSTGRESQL_CONNECTION_STRING"));
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
